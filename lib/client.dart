23187b1984b7e17107e7966a180ad1c224dc1d42
Rasarts <rasart.pro@gmail.com>
2015-07-06 15:25:42 +0300

    update version

--- lib/client.dart
+++ lib/client.dart
@@ -2,9 +2,6 @@ library mini.client;
 
 import 'dart:html';
 import 'package:force/force_browser.dart';
-import 'package:polymer/polymer.dart';
-
-export 'package:polymer/init.dart';
 
 class MiniClient {
   ForceClient WsClient;
